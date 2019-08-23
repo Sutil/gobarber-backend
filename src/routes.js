8043777b9537b9d5e929b26a8bb8a41daf051342
Eduardo <sutil.edu@gmail.com>
2019-08-23 01:28:10 -0300

    Configurações do multer para receber imagens

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import Router from 'express';
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-
+import multerConfig from './config/multer';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -12,5 +14,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
