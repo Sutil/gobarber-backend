d37b3136ce12bba74c2d7bc2a9961afbd3394416
Eduardo <sutil.edu@gmail.com>
2019-08-23 03:07:01 -0300

    Endpoint de listagem de usuários

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import multerConfig from './config/multer';
 import authMiddleware from './app/middlewares/auth';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -15,6 +16,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
